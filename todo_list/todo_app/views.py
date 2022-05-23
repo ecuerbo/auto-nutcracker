e4b80bb8a96cc82586eba5ef0f16bc0c6b61c27a
Elbon <elbon.cuerbo@gmail.com>
2022-05-23 22:46:22 +0800

    before going to bed productivity

--- todo_list/todo_app/views.py
+++ todo_list/todo_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import ToDoList
 
-# Create your views here.
+
+
+class ListListView(ListView):
+    model = ToDoList
+    template_name = "todo_app/index.html"
