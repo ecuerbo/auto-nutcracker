f83a375ef66bd808ac97a713ecb4c04561558777
Elbon <elbon.cuerbo@gmail.com>
2022-08-11 11:43:44 +0800

    start again

--- autosched/chrontest.py
+++
@@ -1,2 +0,0 @@
-with open('chron_test.txt','a') as f:
-    f.write("Hello World!\n")
--- todo_list/todo_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import ListView
-from .models import ToDoList
-
-
-
-class ListListView(ListView):
-    model = ToDoList
-    template_name = "todo_app/index.html"
