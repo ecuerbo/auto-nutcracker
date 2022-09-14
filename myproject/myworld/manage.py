4f39fad6461ded48b2ffd8d8cb5560b68fda3849
Elbon <elbon.cuerbo@gmail.com>
2022-09-14 08:13:39 +0800

    I'm back madapakers

--- myproject/myworld/manage.py
+++ myproject/myworld/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "storefront.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myworld.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- myproject/myworld/myworld/wsgi.py
+++ myproject/myworld/myworld/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for storefront project.
+WSGI config for myworld project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "storefront.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myworld.settings")
 
 application = get_wsgi_application()
