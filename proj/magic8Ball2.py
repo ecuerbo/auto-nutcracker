b06cb488c5868eccde1cd9003f825e5509661901
Elbon <elbon.cuerbo@gmail.com>
2022-11-12 10:14:31 +0800

    good morning

---
+++ proj/magic8Ball2.py
@@ -0,0 +1,14 @@
+import random
+
+messages = ['It is certain',
+'It is decidedly so',
+'Yes definitely',
+'Reply hazy try again',
+'Ask again later',
+'Concentrate and ask again',
+'My reply is no',
+'Outlook not so good',
+'Very doubtful',
+"Fuck off!"]
+
+print(messages[random.randint(0,len(messages)-1)])
